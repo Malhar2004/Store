5f7c61f6fcec70bfbb396c2a83cd0a6d70da9ca7
Malhar2004 <malharmr123@gmail.com>
2024-03-26 11:44:31 +0530

    urls changed

--- StoreSite/urls.py
+++ StoreSite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("StoreApp.urls"))
+    path('api/', include("StoreApp.urls"))
 ]
